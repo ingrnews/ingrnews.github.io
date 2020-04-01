76d7025e4fe2af612bbcf69cfedb9ab65dd62665
GeorgiosKousouris <george.kousouris@gmail.com>
2020-04-01 16:35:19 +0300

    delete

--- final_files/fl_vNysmDJ4.js
+++
@@ -1,3 +0,0 @@
-if (self.CavalryLogger) { CavalryLogger.start_js(["txn55"]); }
-
-__d("isRecruitingCandidatePortal",[],(function(a,b,c,d,e,f){"use strict";function a(){var a;return((a=window)==null?void 0:(a=a.location)==null?void 0:(a=a.hostname)==null?void 0:a.endsWith("facebookrecruiting.com"))||!1}e.exports=a}),null);
